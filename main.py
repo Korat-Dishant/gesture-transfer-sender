c3caea25065aa178e592e320af692443b9384665
Korat Dishant <koratdishant536631@gmail.com>
2024-02-23 15:40:44 +0530

    commit

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ import pyautogui
 
 
 def main():    
-    tunnelUrl = input("Device URL : ")
+    # tunnelUrl = input("Device URL : ")
     # username = input("username")
     # password = input("password")
     username = "master"
     password = "master123"
-    cred = {"tunnelUrl" : tunnelUrl , "username" : username , "password":password}
+    # cred = {"tunnelUrl" : tunnelUrl , "username" : username , "password":password}
 
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_hands = mp.solutions.hands
 
+    horizontal_split_left = 1/3
+    horizontal_split_right = 2/3
 
     cap = cv2.VideoCapture(0)
 
@@ -29,11 +31,14 @@ def main():
         min_detection_confidence=0.4,
         max_num_hands = 1,
         min_tracking_confidence=0.4) as hands:
+        
 
-        while cap.isOpened():
+        while cap.isOpened():   
 
             success, image = cap.read() 
             h, w, c = image.shape
+            left_boundary = int(horizontal_split_left * w)
+            right_boundary = int(horizontal_split_right * w)
             start = time.perf_counter()
             image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
             image.flags.writeable = False
@@ -50,22 +55,46 @@ def main():
                     
                     index_finger_tip_up = hand_landmarks.landmark[8]
                     index_finger_tip_down = hand_landmarks.landmark[6]
-                    
+                    middle_finger_tip_up = hand_landmarks.landmark[12]
+                    middle_finger_tip_down = hand_landmarks.landmark[10] 
+                    thumb_tip_up = hand_landmarks.landmark[4]
+
                     index_finger_up = index_finger_tip_up.y * h
-                    index_finger_down = index_finger_tip_down.y * h
-                    
-                    if index_finger_up < index_finger_down:
-                        cv2.putText(image, "Gas", (500,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-                        # pyautogui.keyDown('right')
-                        # pyautogui.keyUp('left')
-                        # send_action(cred,"right")
-                        print("right")
-                    elif index_finger_up > index_finger_down:
-                        cv2.putText(image, "Brake", (500,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
+                    index_finger_down = index_finger_tip_down.y * h 
+                    middle_finger_up = middle_finger_tip_up.y * h
+                    middle_finger_down = middle_finger_tip_down.y * h
+                    thumb_tip_up = thumb_tip_up.y * h
+
+
+                    # left side 
+                    if (index_finger_tip_up.x * w < left_boundary):
+                        print("L")
+                    # right side 
+                    elif (index_finger_tip_up.x * w > right_boundary):
+                        print("R")
+
+                    # palm close
+                    if index_finger_up > index_finger_down and middle_finger_up > middle_finger_down:
+                        cv2.putText(image, "palm close", (300,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
                         # pyautogui.keyDown('left')
                         # pyautogui.keyUp('right')
                         # send_action(cred,"left")
-                        print("left")
+                        print("PC")
+                    # pinch
+                    elif (thumb_tip_up - index_finger_up) < 30 :
+                        cv2.putText(image, "pinch", (500,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
+                        print("P")
+                    # palm open
+                    elif index_finger_up < index_finger_down and middle_finger_up < middle_finger_down :
+                        cv2.putText(image, "palm open", (300,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+                        # pyautogui.keyDown('right')
+                        # pyautogui.keyUp('left')
+                        # send_action(cred,"right")
+                        print("PO")
+
+
+
+
             else :
                 cv2.putText(image, "null", (500,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,0,0), 2)
                 # pyautogui.keyUp('left')
@@ -77,6 +106,8 @@ def main():
             totalTime = end - start
             fps = 1 / totalTime
             cv2.putText(image, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0),2)
+            cv2.line(image, ( left_boundary, h), ( left_boundary, 0), (255,0,0), 6)
+            cv2.line(image, ( right_boundary, h), ( right_boundary, 0), (255,0,0), 6)
             cv2.imshow('MediaPipe Hands', image)
             if cv2.waitKey(2) == ord('q'):
                 break
